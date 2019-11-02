9867cc8d740266871a6b614375b4f29778413ab2
Dawson Hessler <funkemunkybiz@gmail.com>
2019-11-02 15:17:03 -0400

    Added encrypted field options to Projectcarbon

--- Main/src/main/java/cc/funkemunky/carbon/Carbon.java
+++ Main/src/main/java/cc/funkemunky/carbon/Carbon.java
@@ -1,7 +1,6 @@
 package cc.funkemunky.carbon;
 
 import cc.funkemunky.carbon.db.Database;
-import cc.funkemunky.carbon.utils.security.hash.Hash;
 import lombok.Getter;
 
 import java.util.Map;
@@ -13,10 +12,6 @@ public class Carbon {
     public static Carbon INSTANCE;
 
     private Map<String, Database> databases = new ConcurrentHashMap<>();
-
-    public Carbon() {
-        Hash.loadHashes();
-    }
 
     public <T extends Database> T getDatabase(String name) {
         return (T) databases.get(name);
--- Main/src/main/java/cc/funkemunky/carbon/db/StructureSet.java
+++ Main/src/main/java/cc/funkemunky/carbon/db/StructureSet.java
@@ -1,11 +1,17 @@
 package cc.funkemunky.carbon.db;
 
+import cc.funkemunky.carbon.utils.MiscUtils;
 import cc.funkemunky.carbon.utils.json.JSONException;
 import cc.funkemunky.carbon.utils.json.JSONObject;
+import cc.funkemunky.carbon.utils.security.GeneralUtils;
+import cc.funkemunky.carbon.utils.security.encryption.AES;
+import cc.funkemunky.carbon.utils.security.hash.Hash;
+import cc.funkemunky.carbon.utils.security.hash.HashType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,14 +27,62 @@ public class StructureSet {
         objects.put(key, object);
     }
 
+    public void inputEncryptedField(String key, Object object, String password, HashType type) {
+        Hash hash = Hash.getHashByType(type);
+
+        String hashed = hash.hash(password);
+        try {
+            byte[] encryptedBytes = AES.encrypt(MiscUtils.getBytesOfObject(object), password);
+            String encryptedString = GeneralUtils.bytesToString(encryptedBytes);
+
+            objects.put(key + ":@:" + type.name(), encryptedString + ":@@:" + hashed);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public <T> T getField(String key) {
-        return (T) objects.get(key);
+        if(objects.containsKey(key)) {
+            Object object = objects.get(key);
+
+            try {
+                return (T) object;
+            } catch(ClassCastException e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public <T> T getEncryptedField(String key, String password, HashType type) {
+        if(objects.containsKey((key + ":@:" + type.name()))) {
+            String string = (String) objects.get((key + ":@:" + type.name()));
+            String[] split = string.split(":@@:");
+            String encrypted = split[0];
+            String hashedPass = split[1];
+
+            Hash hash = Hash.getHashByType(type);
+
+            if(hash.hashEqualsKey(hashedPass, password)) {
+
+                byte[] decrypted = AES.decrypt(GeneralUtils.bytesFromString(encrypted), password);
+                try {
+                    return (T) MiscUtils.objectFromBytes(decrypted);
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (ClassCastException e) {
+                    return null;
+                }
+            } else return null;
+        }
+
+        return null;
     }
 
     public boolean containsKey(String key) {
         return objects.containsKey(key);
     }
-
 
     public boolean removeField(String key) {
         if(objects.containsKey(key)) {
